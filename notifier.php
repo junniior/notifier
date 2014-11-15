d80513f703c13f6b5493b1f9bdcb79521d666989
Luan Cuba <luancuba9@gmail.com>
2014-11-15 14:22:31 -0200

    Needed files

---
+++ notifier.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:       Notifier
+ * Plugin URI:        https://github.com/luancuba/notifier
+ * Description:       Create a visual notification to your users
+ * Version:           0.1.0
+ * Author:            Luan Cuba
+ * Author URI:        http://luancuba.com.br
+ * Text Domain:       notifier
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /l10n
+ * GitHub Plugin URI: https://github.com/luancuba/notifier
+ */
